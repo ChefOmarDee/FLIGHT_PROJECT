699593769d757c8a64d91a89f8918934d7da3680
ChefOmarDee <chefomardee@gmail.com>
2022-12-07 15:32:58 -0500

    installed AXIOS and Nodemon

--- client/react-app/src/App.js
+++ client/react-app/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import axios from 'axios';
 function App() {
+
+  async function fetchData() {
+    return axios.get("https://randomuser.me/api/")
+      .then((response) => console.log(response.data));
+  }
+  let result = fetchData();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <p>{result}</p>
+    </React.Fragment>
   );
 }
 
