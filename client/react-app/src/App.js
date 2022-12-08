e175d70cc6869251260ab09d6e7659159cc6cfaa
ChefOmarDee <chefomardee@gmail.com>
2022-12-08 11:23:00 -0500

    Edited API

--- client/react-app/src/App.js
+++ client/react-app/src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
-import React from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+//import axios from 'axios';
 function App() {
-  axios
-    .get("http://127.0.0.1:8080/tickets")
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+  let obj = {}
+  let [arr, setArr] = useState([])
+  var b;
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        `http://127.0.0.1:8080/tickets`
+      )
+      let actualData = await response.json();
+      //console.log(actualData[0])
+      obj = {
+        sourceLocation: actualData[0].sourcelocation,
+        destinationLocation: actualData[0].destinationlocation,
+        sourceDate: actualData[0].sourcedate,
+        destinationDate: actualData[0].destinationdate,
+        passengerCount: actualData[0].passengercount,
+      }
+      setArr([...arr, obj])
+    }
+    getData()
+  }, [])
+  console.log(arr[0])
   return (
     <React.Fragment>
-      <p>jojo</p>
+      <p>afl123{console.log(arr[0])}</p>
     </React.Fragment>
   );
 }
