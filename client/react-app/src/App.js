e5e04f4f0f1d94cff85c0536d9e73eeab259d4cd
ChefOmarDee <chefomardee@gmail.com>
2022-12-08 21:53:52 -0500

    printing json package to the package

--- client/react-app/src/App.js
+++ client/react-app/src/App.js
@@ -1,32 +1,25 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-//import axios from 'axios';
+
 function App() {
-  let obj = {}
-  let [arr, setArr] = useState([])
-  var b;
+
+  let [obj, setObj] = useState([])
   useEffect(() => {
     async function getData() {
-      const response = await fetch(
+      const actualData = await fetch(
         `http://127.0.0.1:8080/tickets`
-      )
-      let actualData = await response.json();
-      //console.log(actualData[0])
-      obj = {
-        sourceLocation: actualData[0].sourcelocation,
-        destinationLocation: actualData[0].destinationlocation,
-        sourceDate: actualData[0].sourcedate,
-        destinationDate: actualData[0].destinationdate,
-        passengerCount: actualData[0].passengercount,
-      }
-      setArr([...arr, obj])
+      ).then(response => response.json())
+      setObj(JSON.stringify(actualData))
     }
     getData()
   }, [])
 
+  //console.log(typeof obj)
+  
+
   return (
     <React.Fragment>
-      <p>afl123{console.log(arr[0])}</p>
+      <p>afl123{obj}</p>
     </React.Fragment>
   );
 }
