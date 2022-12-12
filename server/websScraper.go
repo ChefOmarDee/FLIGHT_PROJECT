1b17932e0778c777504088cc83738684bfefd2a1
ChefOmarDee <chefomardee@gmail.com>
2022-12-11 22:20:15 -0500

    webscraper stores iata codes into a slice.

--- server/websScraper.go
+++ server/websScraper.go
@@ -1,9 +1,31 @@
 package main
-import(
+
+import (
 	"fmt"
 
-	//"github.com/gocolly/colly"
+	"github.com/gocolly/colly"
 )
-func main(){
-fmt.Printf("hello");
+
+var source []string
+var flag int = 1
+
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("bing.com", "www.bing.com"),
+	)
+	c.OnHTML("div.b_focusTextLarge", func(h *colly.HTMLElement) {
+		source = append(source, h.Text)
+	})
+	if flag == 0 {
+		c.OnHTML("div.b_focusTextMedium", func(h *colly.HTMLElement) {
+			source = append(source, h.Text)
+		})
+	}
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Print("we are in\n")
+		flag = 1
+	})
+	c.Visit("https://www.bing.com/search?q=Atlanta,GA iata code")
+	c.Visit("https://www.bing.com/search?q=Valdosta,GA iata code")
+	fmt.Printf("%v", source)
 }
