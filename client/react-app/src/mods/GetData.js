2910dda1bc47364b828ba24367aa8937d221c3cf
Okhan2021 <omar@allclear.ai>
2023-01-17 04:18:53 -0500

    almost completed API

--- client/react-app/src/mods/GetData.js
+++ client/react-app/src/mods/GetData.js
@@ -1,26 +1,45 @@
 import React, { useEffect, useState } from 'react';
 
-let GetData=()=>{
+let GetData=(props)=>{
     let [obj, setObj] = useState([])
+    let [isFormSubmitted,setIsFormSubmitted]=useState(0)
     function timeout(delay) {
       return new Promise( res => setTimeout(res, delay) );
   }
   async function getData() {
-    await timeout(2000)
+    await timeout(6000)
     const actualData = await fetch('http://127.0.0.1:8080/gettickets')
     let jsonActualData=await actualData.json()
     setObj(jsonActualData)
+    setIsFormSubmitted(isFormSubmitted+=1)
+    await console.log(isFormSubmitted)
+
   }
     useEffect(() => {
       getData()
     }, [])
 
+    if(isFormSubmitted>=2){
+      console.log(obj)
+      return(
+        <React.Fragment>
+        <button onClick={getData}>
+        click here for your data
+        </button>
+        <p>
+          {obj.sourcelocation}
+        </p>
+        </React.Fragment>
+        
+      )
+    }
+    // console.log(isFormSubmitted)
     //////////////////////////////////////////
     /////////////////////////////////////////
     return(
         <React.Fragment>
           <button onClick={getData}>
-          {obj.sourcelocation}
+          click here for your data
           </button>
         </React.Fragment>
     )
--- client/react-app/src/mods/WebForm.js
+++ client/react-app/src/mods/WebForm.js
@@ -1,19 +1,8 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState} from 'react'
 import GetData from './GetData'
 let WebForm =()=>{
-    let [obj, setObj] = useState([])
-    function timeout(delay) {
-      return new Promise( res => setTimeout(res, delay) );
-  }
-  async function getData() {
-    const actualData = await fetch('http://127.0.0.1:8080/gettickets')
-    let jsonActualData=await actualData.json()
-    setObj(jsonActualData)
-  }
-    useEffect(() => {
-      getData()
-    }, [])
     console.log("yippie")
+    let [isFormSent,setIsFormSent]=useState(0)
     async function submitHandler(event){
         event.preventDefault();
         let formData={
@@ -31,14 +20,139 @@ let WebForm =()=>{
                 body: JSON.stringify(formData)
             })
             let userFlightDataInJson= await userFlightData.json()
+            setIsFormSent(isFormSent+=1)
+            await console.log(isFormSent)
         }   
-        submitForm(formData)
-        await timeout(6000)
-        getData()
+        await submitForm(formData)
+    }
+        if(isFormSent){
+            return (
+                <React.Fragment>
+                    <form onSubmit={submitHandler}>
+                        <label >Enter Flight Origin City</label>
+                        <input type="text"></input>
+                        <label>Enter Flight Origin State</label>
+                        <select>
+                            <option value="AL">AL</option>
+                            <option value="AK">AK</option>
+                            <option value="AR">AR</option>
+                            <option value="AZ">AZ</option>
+                            <option value="CA">CA</option>
+                            <option value="CO">CO</option>
+                            <option value="CT">CT</option>
+                            <option value="DC">DC</option>
+                            <option value="DE">DE</option>
+                            <option value="FL">FL</option>
+                            <option value="GA">GA</option>
+                            <option value="HI">HI</option>
+                            <option value="IA">IA</option>
+                            <option value="ID">ID</option>
+                            <option value="IL">IL</option>
+                            <option value="IN">IN</option>
+                            <option value="KS">KS</option>
+                            <option value="KY">KY</option>
+                            <option value="LA">LA</option>
+                            <option value="MA">MA</option>
+                            <option value="MD">MD</option>
+                            <option value="ME">ME</option>
+                            <option value="MI">MI</option>
+                            <option value="MN">MN</option>
+                            <option value="MO">MO</option>
+                            <option value="MS">MS</option>
+                            <option value="MT">MT</option>
+                            <option value="NC">NC</option>
+                            <option value="NE">NE</option>
+                            <option value="NH">NH</option>
+                            <option value="NJ">NJ</option>
+                            <option value="NM">NM</option>
+                            <option value="NV">NV</option>
+                            <option value="NY">NY</option>
+                            <option value="ND">ND</option>
+                            <option value="OH">OH</option>
+                            <option value="OK">OK</option>
+                            <option value="OR">OR</option>
+                            <option value="PA">PA</option>
+                            <option value="RI">RI</option>
+                            <option value="SC">SC</option>
+                            <option value="SD">SD</option>
+                            <option value="TN">TN</option>
+                            <option value="TX">TX</option>
+                            <option value="UT">UT</option>
+                            <option value="VT">VT</option>
+                            <option value="VA">VA</option>
+                            <option value="WA">WA</option>
+                            <option value="WI">WI</option>
+                            <option value="WV">WV</option>
+                            <option value="WY">WY</option>
+                        </select>
+                        <label >Enter Flight Destination City</label>
+                        <input type="text"></input>
+                        <label>Enter Flight Destination State</label>
+                        <select>
+                            <option value="AL">AL</option>
+                            <option value="AK">AK</option>
+                            <option value="AR">AR</option>
+                            <option value="AZ">AZ</option>
+                            <option value="CA">CA</option>
+                            <option value="CO">CO</option>
+                            <option value="CT">CT</option>
+                            <option value="DC">DC</option>
+                            <option value="DE">DE</option>
+                            <option value="FL">FL</option>
+                            <option value="GA">GA</option>
+                            <option value="HI">HI</option>
+                            <option value="IA">IA</option>
+                            <option value="ID">ID</option>
+                            <option value="IL">IL</option>
+                            <option value="IN">IN</option>
+                            <option value="KS">KS</option>
+                            <option value="KY">KY</option>
+                            <option value="LA">LA</option>
+                            <option value="MA">MA</option>
+                            <option value="MD">MD</option>
+                            <option value="ME">ME</option>
+                            <option value="MI">MI</option>
+                            <option value="MN">MN</option>
+                            <option value="MO">MO</option>
+                            <option value="MS">MS</option>
+                            <option value="MT">MT</option>
+                            <option value="NC">NC</option>
+                            <option value="NE">NE</option>
+                            <option value="NH">NH</option>
+                            <option value="NJ">NJ</option>
+                            <option value="NM">NM</option>
+                            <option value="NV">NV</option>
+                            <option value="NY">NY</option>
+                            <option value="ND">ND</option>
+                            <option value="OH">OH</option>
+                            <option value="OK">OK</option>
+                            <option value="OR">OR</option>
+                            <option value="PA">PA</option>
+                            <option value="RI">RI</option>
+                            <option value="SC">SC</option>
+                            <option value="SD">SD</option>
+                            <option value="TN">TN</option>
+                            <option value="TX">TX</option>
+                            <option value="UT">UT</option>
+                            <option value="VT">VT</option>
+                            <option value="VA">VA</option>
+                            <option value="WA">WA</option>
+                            <option value="WI">WI</option>
+                            <option value="WV">WV</option>
+                            <option value="WY">WY</option>
+                        </select>
+                        <label >Enter Flight Date</label>
+                        <input type="date" defaultValue= {new Date().toISOString().split('T')[0].toString()} max={new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toISOString().split('T')[0].toString()} min={new Date().toISOString().split('T')[0].toString()} required pattern="\d{2}-\d{2}-\d{4}"/>
+                        <label>Enter Flight Passenger Count</label>
+                        <input type="text" ></input>
+                        <input type="submit"></input>
+                    </form>  
+                    <GetData></GetData>
+                </React.Fragment>
+                )
     }
     return (
     <React.Fragment>
-        {obj.flightprice}
         <form onSubmit={submitHandler}>
             <label >Enter Flight Origin City</label>
             <input type="text"></input>
